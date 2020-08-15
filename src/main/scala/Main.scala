806fc4cb58545efbef16d1e18e9dc6f46be35cb3
mkusaka <hinoshita1992@gmail.com>
2020-08-15 22:03:51 +0900

    build

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,12 @@
+import java.util.function.Consumer
+import javafx.animation.{KeyFrame, KeyValue, Timeline}
 import javafx.application.Application
-import javafx.event.{ActionEvent, EventHandler}
-import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.layout.StackPane
+import javafx.scene.{Group, Node, Scene}
+import javafx.scene.paint.Color
+import javafx.scene.shape.{Circle, StrokeType}
 import javafx.stage.Stage
+import javafx.util.Duration
+import java.lang.Math.random
 
 object Main extends App {
   Application.launch(classOf[Main], args: _*)
@@ -11,20 +14,38 @@ object Main extends App {
 
 class Main extends Application {
   override def start(primaryStage: Stage): Unit = {
-    val btn = new Button()
-    btn.setText("押してね")
-    btn.setOnAction(new EventHandler[ActionEvent] {
-      override def handle(event: ActionEvent): Unit = {
-        println("こんにちは")
-      }
-    })
-
-    val root = new StackPane()
-    root.getChildren.add(btn)
-
-    val scene = new Scene(root, 300, 250)
-    primaryStage.setTitle("コンソールにこんにちはを入力")
+    val root = new Group()
+    val scene = new Scene(root, 800, 600, Color.BLACK)
     primaryStage.setScene(scene)
+
+    val circles = new Group()
+    for (i <- 1 to 30) {
+      val circle = new Circle(150, Color.web("white", 0.05))
+      circle.setStrokeType(StrokeType.OUTSIDE)
+      circle.setStroke(Color.web("white", 0.16))
+      circle.setStrokeWidth(4)
+      circles.getChildren().add(circle)
+    }
+    root.getChildren().add(circles)
+
+    val timeline = new Timeline()
+    circles.getChildren().forEach(new Consumer[Node] {
+
+      override def accept(circle: Node): Unit = {
+        timeline.getKeyFrames().addAll(
+          new KeyFrame(Duration.ZERO,
+            new KeyValue(circle.translateXProperty(), random() * 800: Number),
+            new KeyValue(circle.translateYProperty(), random() * 600: Number)
+          ),
+          new KeyFrame(new Duration(40000),
+            new KeyValue(circle.translateXProperty(), random() * 800: Number),
+            new KeyValue(circle.translateYProperty(), random() * 600: Number)
+          )
+        )
+      }
+
+    })
+    timeline.play()
     primaryStage.show()
   }
 }
