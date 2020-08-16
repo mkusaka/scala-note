e76acf79c41bfebbe974c8bd0f49719d1d8102ef
mkusaka <hinoshita1992@gmail.com>
2020-08-16 19:34:55 +0900

    init nightcore player target

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "fuga"
+name := "scala-note"
 
 version := "0.1"
 
--- src/main/scala/jp/co/dwango/marubatsu/board/CellState.scala
+++
@@ -1,6 +0,0 @@
-package jp.co.dwango.marubatsu.board
-
-sealed abstract class CellState
-case object Empty extends CellState
-case object Maru  extends CellState
-case object Batsu extends CellState
